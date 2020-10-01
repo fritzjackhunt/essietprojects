e7bba9304cb0f9affc2bf46df58be6a86500fdb6
fritzjackhunt <essiet.aniekan@yahoo.com>
2020-10-02 00:30:13 +0300

    First commit

---
+++ essiet/views.py
@@ -0,0 +1,11 @@
+# from django.shortcuts import render
+
+from django.http import HttpResponse
+from django.template import loader, context
+from django.shortcuts import render
+import json
+
+
+
+def home(request):
+    return render(request, 'essietproject/base.html')
