cc05fe582baca64fac797a0552c3a561e911f719
fritzjackhunt <essiet.aniekan@yahoo.com>
2021-01-09 20:18:08 +0300

    update

--- users/views.py
+++ users/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
+from .forms import SignUpForm, EditProfileForm 
 
 class UserRegisterView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpForm
     template_name = 'registration/registration.html'
     success_url = reverse_lazy('login')
+
+class UserEditView(generic.UpdateView):
+    form_class = EditProfileForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('homepage')
+
+    def get_object(self):
+        return self.request.user
