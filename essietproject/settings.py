9698f348c76b5cb6da3bd0e382e4d4c01b93f4dc
fritzjackhunt <essiet.aniekan@yahoo.com>
2021-01-31 20:35:48 +0300

    update

--- essietproject/settings.py
+++ essietproject/settings.py
@@ -35,9 +35,9 @@ with open('essietproject SK.txt') as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1:8000"]
 
 
 #    server {
