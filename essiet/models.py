6b335f086ce21d91bc2bc192ef0e4062ccd5c3ab
fritzjackhunt <essiet.aniekan@yahoo.com>
2021-02-22 15:19:22 +0300

    subscribed

--- essiet/models.py
+++ essiet/models.py
@@ -4,9 +4,9 @@ from .utils import SendSubscribeMail
 from django.utils import timezone
 
 
-class Subscribe(models.Model):
-    email_id = models.EmailField(null = True, blank = True)
-    timestamp = models.DateTimeField(default=timezone.now)
+#class Subscribe(models.Model):
+ #   email_id = models.EmailField(null = True, blank = True)
+  #  timestamp = models.DateTimeField(default=timezone.now)
 	
-    def __str__(self):
-		    return self.email_id
+  #  def __str__(self):
+	#	    return self.email_id
